43f95c2812f7ed00b0af32ec55148967de6b271c
bootcamp2024 <bruno.bm12@gmail.com>
2025-03-12 12:38:36 -0300

    GameMinProjection

--- src/main/java/com/portbrunobraatz/dslist/DTO/GameMinDTO.java
+++ src/main/java/com/portbrunobraatz/dslist/DTO/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.portbrunobraatz.dslist.DTO;
 
 import com.portbrunobraatz.dslist.entities.Game;
+import com.portbrunobraatz.dslist.projections.GameMinProjection;
 
 
 public class GameMinDTO {
@@ -20,6 +21,14 @@ public class GameMinDTO {
         this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+        this.shortDescription = projection.getShortDescription();
     }
 
     //Getters
--- src/main/java/com/portbrunobraatz/dslist/controllers/GameListController.java
+++ src/main/java/com/portbrunobraatz/dslist/controllers/GameListController.java
@@ -3,8 +3,10 @@ package com.portbrunobraatz.dslist.controllers;
 import com.portbrunobraatz.dslist.DTO.GameListDTO;
 import com.portbrunobraatz.dslist.DTO.GameMinDTO;
 import com.portbrunobraatz.dslist.sevices.GameListService;
+import com.portbrunobraatz.dslist.sevices.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
 
